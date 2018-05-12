0117c505e318d2f5137fd477d106a39aca294168
cdmunoz <_a_villa81_>
2018-05-12 11:35:16 -0500

    Fixes in build.gradle notations and changes in the sources for the list item (url)

--- app/src/main/java/co/cdmunoz/kotlintrainingapp/Item.kt
+++ app/src/main/java/co/cdmunoz/kotlintrainingapp/Item.kt
@@ -1,7 +1,7 @@
 package co.cdmunoz.kotlintrainingapp
 
 fun getItems(): List<Item> {
-  return (1..10).map { Item(it.toLong(), "Title $it", "http://lorempixel.com/400/400/cats/$it/") }
+  return (10..19).map { Item(it.toLong(), "Image $it", "https://picsum.photos/400/400/?image=$it") }
 }
 
 data class Item(val id: Long, val title: String, val url: String)
